e5c9faf7ce9b6f6e865b9ed302c1679f8d98dd8b
lethanhtin2905 <56227170+ngocthuc37@users.noreply.github.com>
2021-06-08 22:03:56 +0700

    Add func update status for order

--- controllers/order-controllers.js
+++ controllers/order-controllers.js
@@ -12,3 +12,21 @@ exports.displayOrder = (req, res) => {
             throw err;
         });
 }
+
+exports.updateOrder = (req, res) => {
+    var id = req.query.id;
+    //console.log(id);
+    var status = req.query.status;
+    var statusNumber = parseInt(status, 10);
+
+
+    if (statusNumber != -1) {
+        Order.updateOne({ _id: id }, {
+            $set: {
+                status: status,
+            }
+        }).then(res.redirect('/order'));
+    } else {
+        res.redirect('/order')
+    }
+}
