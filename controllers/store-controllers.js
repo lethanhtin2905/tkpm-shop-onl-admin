4e87b526a3e2a202e42395369058bc7602a4eebd
lethanhtin2905 <57804580+lethanhtin2905@users.noreply.github.com>
2021-07-08 20:07:31 +0700

    Update store

--- controllers/store-controllers.js
+++ controllers/store-controllers.js
@@ -61,52 +61,6 @@ exports.addStore = async function (req, res, next) {
    )
 }
 
-// Edit store
-/*exports.editStore = (req, res) => {
-   console.log(req.body)
-   var name = req.body.name;
-   var describe = req.body.describe;
-   var address = req.body.address;
-
-   console.log("Edit store: ")
-   console.log("name:", name);
-   console.log("describe:", describe);
-   console.log("address:", address);
-
-   if (!req.file) {
-      res.status(401).json({ error: 'Please provide an image...!' });
-   }
-   const uniqueFilename = new Date().toISOString();
-   const cloudinary = require('cloudinary').v2;
-   cloudinary.config({
-      cloud_name: 'lethanhtin',
-      api_key: '259864364255718',
-      api_secret: '8PaulhQKCv3BCW6SdXMHu5NHSzY'
-   })
-   cloudinary.uploader.upload(
-      "data:image/png;base64," + (req.file.buffer).toString('base64'),
-      { public_id: 'Electro/' + uniqueFilename, tags: 'store' }, // directory and tags are optional
-      function (err, image) {
-         if (err) {
-            return res.send(err);
-         }
-         Store.update({
-            name: name,
-            image: image.url,
-            describe: describe,
-            address: address
-         },
-         function (err, small) {
-            if (err) return next(err);
-            else {
-               res.redirect('/store');
-            }
-         });
-         console.log(image);
-      }
-   )
-}*/
-
 // Delete store
 exports.deleteStore = function (req, res, next) {
    const id = req.params.id;
