b88aea2277574924a9e0c011270e4de6c3efd3d6
lethanhtin2905 <57804580+lethanhtin2905@users.noreply.github.com>
2021-07-09 22:11:36 +0700

    final_ver1

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// set up =======================================================
+// Set up
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -13,15 +13,13 @@ var createError = require('http-errors');
 
 var app = express();
 
-// router files =================================================
+// Router files
 var indexRouter = require('./routes/index');
 var adminRouter = require('./routes/admin');
 var Database = require('./db/database');
 
-// Passport config ==============================================
+// Passport config
 require('./config/passport')(passport);
-
-
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -41,9 +39,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 // Static path
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/category', express.static(path.join(__dirname, 'public')));
 app.use('/login', express.static(path.join(__dirname, 'public')));
-
 
 // Express Session
 app.use(session({
@@ -71,18 +67,18 @@ app.use((req, res, next) => {
 app.use('/', indexRouter);
 app.use('/', adminRouter);
 
-// catch 404 and forward to error handler
+// Catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// Error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+  // Set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  // Render the error page
   res.status(err.status || 500);        
   res.render('error');
 });
